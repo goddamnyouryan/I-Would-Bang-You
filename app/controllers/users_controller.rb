ce5f364c6df58a83f6cfc8e1b12aa5f556aa782a
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-12-09 01:32:40 -0800

    Adding 404 page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except => [ :index, :check_login, :contact, :about ]
+  before_filter :authenticate_user!, :except => [ :index, :check_login, :contact, :learn_more ]
   after_filter :add_visit, :only => [:show, :random]
   
   def index
